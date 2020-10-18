6450550a201fb2883b941da82f764b99b281bc07
Artur Szwagrzak <artur@szwagrzak.pl>
2020-10-18 02:14:34 +0200

    recreate migrations; add opinion tree images and styles

--- opinions/models.py
+++ opinions/models.py
@@ -5,9 +5,9 @@ from djrichtextfield.models import RichTextField
 class OpinionsConfiguration(models.Model):
     app_name = "Opinions"
     main_image = models.ImageField(upload_to='images/', help_text='Obrazek wyswietlany na gorze strony pod menu')
-    main_image_alt = models.CharField(max_length=400,
-                                      help_text='Tekst wyświetlany w przypadku gdyby obrazek sie nie zaladowal')
+    main_image_alt = models.CharField(max_length=400, help_text='Tekst wyswietlany w przypadku gdyby obrazek sie nie zaladowal')
     update_date = models.DateTimeField('modification date', auto_now=True)
+    tree_img = models.ImageField(upload_to='images/', help_text='Obrazek wyswietlany jako głowny w drzewku')
 
     def __str__(self):
         return self.app_name + " page configuration"
