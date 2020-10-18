bf78e3b185f88c978276d4c18a18daaa1d0f0621
Artur Szwagrzak <artur@szwagrzak.pl>
2020-10-18 15:01:38 +0200

    add slick slider to project, add one to opinions

---
+++ demosite/static/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $('#customer-opinions').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+  });
+  console.log('ready!');
+});
