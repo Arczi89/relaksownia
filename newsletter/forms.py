97555255e7c88099321ffba0dc9b2abec9f14b04
Artur Szwagrzak <artur@szwagrzak.pl>
2020-10-19 23:49:10 +0200

    translate labels in newsletter form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from .models import Newsletter
 from bootstrap_modal_forms.forms import BSModalModelForm
 
@@ -5,4 +7,9 @@ from bootstrap_modal_forms.forms import BSModalModelForm
 class NewsletterForm(BSModalModelForm):
     class Meta:
         model = Newsletter
+        labels = {
+            'email': _('Adres e-mail'),
+            'name': _('Imię'),
+            'permission': _('Wyrażam zgodę na przesyłanie mi ofert marketingowych i promocyjnych drogą elektroniczną'),
+        }
         fields = "__all__"
