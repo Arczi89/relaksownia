691cb75979d775f09a4fcd6d12c9f4cd793d8a53
Artur Szwagrzak <artur@szwagrzak.pl>
2020-12-04 22:10:22 +0100

    change contact form validation messages to more detailed

--- demosite/constants.py
+++ demosite/constants.py
@@ -7,4 +7,5 @@ email_or_phone_required = 'Uzupełnij pole email albo telefon'
 incorrect_email_format = 'Niepoprawny email'
 incorrect_phone_format = 'Niepoprawny numer telefonu, numer powinien mieć 9 cyfr'
 incorrect_extension = 'Nieprawidłowe rozszerzenie pliku'
-
+field_name_required = 'Pole Imię jest wymagane'
+field_message_required = 'Pole Wiadomość jest wymagane'
