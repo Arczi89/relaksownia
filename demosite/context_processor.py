d4dd9f7dacd1afb59b85321582e98800f08b1cd2
Artur Szwagrzak <artur@szwagrzak.pl>
2020-12-12 04:01:01 +0100

    make social links editable #15

--- demosite/context_processor.py
+++ demosite/context_processor.py
@@ -12,3 +12,33 @@ def contact_phone_processor(request):
     return {
         'contact_phone': contact_phone
     }
+
+
+def social_media_processor(request):
+    # For _footer.html template file
+    try:
+        configuration = ContactConfiguration.objects.all()[:1].get()
+        facebook = configuration.facebook
+        twitter = configuration.twitter
+        linkedin = configuration.linkedin
+        instagram = configuration.instagram
+        snapchat = configuration.snapchat
+        youtube = configuration.youtube
+        google_plus = configuration.google_plus
+    except ContactConfiguration.DoesNotExist:
+        facebook = None
+        twitter = None
+        linkedin = None
+        instagram = None
+        snapchat = None
+        youtube = None
+        google_plus = None
+    return {
+        'facebook': facebook,
+        'twitter': twitter,
+        'linkedin': linkedin,
+        'instagram': instagram,
+        'snapchat': snapchat,
+        'youtube': youtube,
+        'google_plus': google_plus
+    }
