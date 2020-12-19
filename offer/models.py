82778ec2c9a103e1608dde15194d9ba777f65c8b
Artur Szwagrzak <artur@szwagrzak.pl>
2020-12-19 14:48:15 +0100

    fix element_order in __str__ functions

--- offer/models.py
+++ offer/models.py
@@ -12,7 +12,7 @@ class OfferItem(models.Model):
     element_order = models.IntegerField(default=0, verbose_name=_('Kolejnosc'))
 
     def __str__(self):
-        return self.title + "(" + self.element_order + ")"
+        return '%s ( %i )' % (self.title, self.element_order)
 
     class Meta:
         verbose_name = _('Oferta')
