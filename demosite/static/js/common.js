930d0b2bed1bb55cfb03b62b9dae7f43c72fbbea
Artur Szwagrzak <artur@szwagrzak.pl>
2020-12-30 03:29:33 +0100

    responsive design - part 1 #6

--- demosite/static/js/common.js
+++ demosite/static/js/common.js
@@ -7,6 +7,30 @@ $(document).ready(function() {
         slidesToScroll: 3,
         dots: true,
         adaptiveHeight: false,
+        responsive: [
+                        {
+                          breakpoint: 1100,
+                          settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1
+                          }
+                        },
+                        {
+                          breakpoint: 768,
+                          settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                          }
+                        },
+                                                {
+                          breakpoint: 320,
+                          settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: false,
+                          }
+                        },
+                      ]
     });
 
     console.log('ready!');
