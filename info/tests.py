153127638805c7d8e1fabb6d4cafd3ae3e99f046
Artur Szwagrzak <artur@szwagrzak.pl>
2021-01-04 21:11:09 +0100

    fix problem with blog migration update_date + add cert items model and test #45

--- info/tests.py
+++ info/tests.py
@@ -1,3 +1,84 @@
+import tempfile
+from http import HTTPStatus
+
+from django.template.loader import render_to_string
 from django.test import TestCase
 
-# Create your tests here.
+
+# TODO+ django data driven test - taki sam kształt testu, zmieniają się tylko dane
+# TODO+ assertPy - biblioteka do asercji
+# TODO+ klasa bazowa z metodami setupowymi do tworzenia wspólnych obiektów
+from .models import Info, CertItem
+
+
+class InfoTests(TestCase):
+
+    def addBasicConfigurations(self):
+        info_configuration = Info(
+            photo=tempfile.NamedTemporaryFile(suffix=".jpg").name,
+            photo_alt="main_image_alt",
+            description="Lorem ipsum description, test test test"
+        )
+        info_configuration.save()
+        return info_configuration.pk
+
+    def addCertItem(self, image_alt, order=0):
+        cert_item = CertItem(
+            image=tempfile.NamedTemporaryFile(suffix=".jpg").name,
+            image_alt=image_alt,
+            element_order=order
+        )
+        cert_item.save()
+
+    def removeMainConfiguration(self):
+        obj = Info.objects.get(pk=self.info_configuration_pk)
+        obj.delete()
+
+    def setUp(self):
+        self.baseUrl = "/info/"
+        self.info_configuration_pk = self.addBasicConfigurations()
+
+    def test_info_page_with_configuration_loaded(self):
+        self.addCertItem("certificate 1", 1)
+        response = self.client.get(self.baseUrl)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_info_page_without_configuration_loaded(self):
+        self.removeMainConfiguration()
+        response = self.client.get(self.baseUrl)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_elements_are_in_correct_order(self):
+        # Arrange
+        self.addCertItem("certificate 1", 0)
+        self.addCertItem("certificate 2", 2)
+        self.addCertItem("certificate 3", 1)
+        # Act
+        response = self.client.get(self.baseUrl)
+        # Assert
+        self.assertLessEqual(response.context['certs'][0].element_order, response.context['certs'][1].element_order, "There is wrong order of elements")
+        self.assertLessEqual(response.context['certs'][1].element_order, response.context['certs'][2].element_order, "There is wrong order of elements")
+
+    def test_elements_with_the_same_order_number_are_correct_ordered(self):
+        # Arrange
+        self.addCertItem("certificate 1", 7)
+        self.addCertItem("certificate 2", 7)
+        self.addCertItem("certificate 3", 4)
+        # Act
+        response = self.client.get(self.baseUrl)
+        # Assert
+        self.assertLessEqual(response.context['certs'][0].element_order, response.context['certs'][1].element_order, "There is wrong order of elements")
+        self.assertLessEqual(response.context['certs'][1].element_order, response.context['certs'][2].element_order, "There is wrong order of elements")
+
+    def test_should_all_section_be_visible_in_page(self):
+        # Arrange
+        template_name = "info.html"
+        context = {
+            "about_me": "not empty",
+            "certs": "not empty"
+        }
+        # Act
+        result = render_to_string(template_name, context)
+        # Assert
+        self.assertTrue("about-me-container" in result, "there is no 'about me' container on page")
+        self.assertTrue("certs-container" in result, "there is no certification container on page")
--- info/views.py
+++ info/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 
-from .models import Info
+from .models import Info, CertItem
 
 
 def info(request):
     try:
-        # TODO+ zmienilem (w info) na configuration po zmianie modelu
         about_me = Info.objects.all()[:1].get()
     except Info.DoesNotExist:
         about_me = None
+    try:
+        certs = CertItem.objects.order_by('element_order')
+    except CertItem.DoesNotExist:
+        certs = None
+
     context = {
-        'about_me': about_me
+        'about_me': about_me,
+        'certs': certs
     }
     return render(request, 'info.html', context)
