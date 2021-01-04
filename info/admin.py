1cfafa96c30a157e761cdd540120adaf3ef33b90
Artur Szwagrzak <artur@szwagrzak.pl>
2021-01-04 21:13:50 +0100

    add cert items to admin panel #45

--- info/admin.py
+++ info/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Info
+from .models import Info, CertItem
 
 
 @admin.register(Info)
 class InfoAdmin(admin.ModelAdmin):
+    save_as = True
+
+
+@admin.register(CertItem)
+class CertItemAdmin(admin.ModelAdmin):
     save_as = True
