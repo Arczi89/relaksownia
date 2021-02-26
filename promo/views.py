5e9761c22d7e5af4070fa1a0bb743747d1db4ef7
Artur Szwagrzak <artur@szwagrzak.pl>
2021-02-26 17:16:08 +0100

    promo form - add html and basic styling

--- promo/views.py
+++ promo/views.py
@@ -1,11 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
 
+from .forms import PromoClientForm
 from .models import PromoPageComponent
 
 
 def promo(request):
+    if request.method == "POST":
+        form = PromoClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Twoja wiadomość została wysłana.')
+            return HttpResponseRedirect('/contact/')
+    else:
+        form = PromoClientForm()
+
     promo_items = PromoPageComponent.objects.order_by("element_order")
     context = {
-        'promoItems': promo_items
+        'promoItems': promo_items,
+        "form": form
     }
     return render(request, 'promo.html', context)
