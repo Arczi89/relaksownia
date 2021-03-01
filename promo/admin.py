eb86b4121f6eb43e3759ed29e91d418216c7f6f1
Artur Szwagrzak <artur@szwagrzak.pl>
2021-03-01 17:52:02 +0100

    add promo configuration and promoclient to admin panel

--- promo/admin.py
+++ promo/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import PromoPageComponent
+from .models import PromoPageComponent, PromoConfiguration, PromoClient
 
 
 @admin.register(PromoPageComponent)
 class PromoItemAdmin(admin.ModelAdmin):
     save_as = True
+
+
+@admin.register(PromoConfiguration)
+class PromoItemAdmin(admin.ModelAdmin):
+    save_as = True
+
+
+@admin.register(PromoClient)
+class PromoItemAdmin(admin.ModelAdmin):
+    save = False
+    save_as = False
