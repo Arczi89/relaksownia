f2abc3ed2a51edcaea8c8f67b09a84cc12fe3910
Artur Szwagrzak <artur@szwagrzak.pl>
2021-04-13 21:22:21 +0200

    fix email password privacy

--- demosite/settings.tst.py
+++ demosite/settings.tst.py
@@ -192,3 +192,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'demosite/static')
 ]
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'arturszwagrzak.atthost24.pl'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'artur@wizytoowka.pl'
+EMAIL_HOST_PASSWORD = os.environment.get('EMAIL_HOST_PASSWORD')
