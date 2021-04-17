caaa8951f333e6264af67b17163470f3e6199ffa
Artur Szwagrzak <artur@szwagrzak.pl>
2021-04-17 15:09:29 +0200

    add mailing system to newsletter (modal + in faq)

--- faq/views.py
+++ faq/views.py
@@ -2,8 +2,11 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from post_office.mail import logger
+from post_office import mail
 
 from main.models import MainConfiguration
+from promo.models import PromoEmailConfiguration
 from .forms import NewsletterFaqForm
 from .models import FaqItem, FaqConfiguration
 
@@ -39,5 +42,63 @@ def saveForm(request):
     form = NewsletterFaqForm(request.POST)
     if form.is_valid():
         store_newsletter_request(form)
-        messages.add_message(request, messages.SUCCESS, 'Zostałeś dodany do listy newslettera.')
+        is_sent = send_email_to_customer(form)
+        send_email_to_admin(form)
+        show_status_message(is_sent, request)
         return HttpResponseRedirect(reverse('faq'))
+
+
+def show_status_message(is_sent, request):
+    if is_sent:
+        messages.add_message(request, messages.SUCCESS, 'Zostałeś zapisany na newsletter, dostaniesz email potwierdzający.')
+    else:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Wysyłanie twojej wiadomości się nie powiodło. Skontaktuj się bezpośrednio pod adres: ' + self.get_email_configuration().from_email
+        )
+
+
+def get_email_configuration():
+    try:
+        config = PromoEmailConfiguration.objects.all()
+        email_configuration = config[:1].get()
+    except PromoEmailConfiguration.DoesNotExist:
+        email_configuration = PromoEmailConfiguration()
+    return email_configuration
+
+
+def send_email_to_customer(form):
+    recipient = form['email'].data
+    if recipient:
+        mail.send(
+            [recipient],
+            sender=get_email_configuration().from_email,
+            template="potwierdzenie_newsletter",
+            priority='now',
+            context={
+                'imie': form['name'].data,
+                'email': form['email'].data,
+            }
+        )
+        return True
+    else:
+        logger.error("Wiadomość nie została wysłana do klienta, dane klienta: " + json.dumps(form))
+        return False
+
+
+def send_email_to_admin(form):
+    recipient = form['email'].data
+    if recipient:
+        mail.send(
+            [get_email_configuration().admin_email],
+            sender=get_email_configuration().from_email,
+            template='potwierdzenie_newsletter_admin',
+            priority='now',
+            context={
+                'imie': form['name'].data,
+                'email': form['email'].data,
+            }
+        )
+    else:
+        logger.error("Wiadomość nie została wysłana do admina, dane klienta: " + json.dumps(form))
