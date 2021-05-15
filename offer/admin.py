9510ef4b9527a849e9863f6c98bc10a2328c6849
Artur Szwagrzak <artur@szwagrzak.pl>
2021-05-15 15:33:58 +0200

    add editable main-text in offer page

--- offer/admin.py
+++ offer/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import OfferItem
+from .models import OfferItem, OfferConfiguration
+
+
+@admin.register(OfferConfiguration)
+class OfferConfigurationAdmin(admin.ModelAdmin):
+    save_as = True
 
 
 @admin.register(OfferItem)
 class OfferItemAdmin(admin.ModelAdmin):
     save_as = True
-
